b1c5338eb4cff0f0def8490e86a7ec91820a4767
Joseph Khawly <joseph.khawly@gmail.com>
2015-07-14 11:30:10 -0400

    Created spare class file for pass management, just in case we need it.

---
+++ BizPass/PassControl.swift
@@ -0,0 +1,14 @@
+//
+//  PassControl.swift
+//  BizPass
+//
+//  Created by Joseph Khawly on 7/14/15.
+//  Copyright (c) 2015 Joseph Khawly. All rights reserved.
+//
+
+import UIKit
+import PassKit
+
+class PassControl {
+    
+}
--- BizPass/ViewController.swift
+++ BizPass/ViewController.swift
@@ -19,6 +19,8 @@ import PassKit
                     init(pass)
 
         Think about using a table view instead.
+
+        Feature suggestion: have the user choose whatever info that isn't their name and title to present on the front of the card.
      */
 
 class ViewController: UIViewController {
@@ -29,19 +31,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleField: UITextField!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var twitterField: UITextField!
-    @IBOutlet weak var webstieField: UITextField!
+    @IBOutlet weak var websiteField: UITextField!
     @IBOutlet weak var linkedinField: UITextField!
     @IBOutlet weak var resumeField: UITextField!
     @IBOutlet weak var githubField: UITextField!
     
+    
+    //If this method gets too crowded, we'll put the pass handling in a separate class.
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Check if the user's device has Passbook.
         if PKPassLibrary.isPassLibraryAvailable() {
+            
+            //If they do, start the PassSlot service.
             PassSlot.start("oalzIWdtIUZdOmQaaCtSQnRYzrHpFjSLeiLjBXiLlzMLOPLFmeoYYmvUljKyMacX")
+            
+            /*
+            
+            the method for creating a pass.
+            
+            PassSlot.createPassFromTemplate(<#template: PSPassTemplate!#>, withValues: <#[NSObject : AnyObject]!#>, withImages: <#[AnyObject]!#>, andRequestInstallation: <#UIViewController!#>, completion: <#PSCompletion!##() -> Void#>)
+            */
             
         } else {
             
+            // If they don't, present an alert controller and exit the app.
+            let alertController = UIAlertController(title: "Well shit, son.", message: "Looks like you don't have Passbook.", preferredStyle: .Alert)
+            let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            alertController.addAction(okAction)
+            self.presentViewController(alertController, animated: true, completion: nil)
         }
     }
 
