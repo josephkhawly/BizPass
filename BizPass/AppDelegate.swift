ae8930d6705c8a1ae445266269a307dfaf7cf2e9
Joseph Khawly <joseph.khawly@gmail.com>
2015-07-22 17:06:11 -0400

    Organized class files

--- BizPass/AppDelegate.swift
+++ BizPass/AppDelegate.swift
@@ -19,7 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         //Start the PassSlot service.
         PassSlot.start("sVSUwmfkUQdmOsFjZPvFqmeUqQPeLqnhthejrVRVwgBNbWUFLOtfwlFUSWRuvdQQ")
-        
         return true
     }
 
@@ -44,9 +43,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-    
-    
-
-
 }
 
