d86d29ef474e4f8d4fe5d3da66f9003c6d7fcd4b
Joseph Khawly <joseph.khawly@gmail.com>
2015-11-08 20:36:56 -0500

    Initial Commit

---
+++ BizPass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BizPass
+//
+//  Created by Joseph Khawly on 11/8/15.
+//  Copyright © 2015 Joseph Khawly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
